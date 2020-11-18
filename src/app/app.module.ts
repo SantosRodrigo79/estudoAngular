c0676f5cf482d519f17afdd933eb6719695016d2
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-11-17 22:08:38 -0300

    Criando tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/Forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
