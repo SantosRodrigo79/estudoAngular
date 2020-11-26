62963fe51c616fe9b6db3ef2dba76bc642e902a5
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-11-26 13:09:20 -0300

    puxandoImagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,17 @@ import{ JsonService } from './json.service';
 })
 export class AppComponent {
   title = 'Minha Página';
-
+  data:string = '';
+  skinsDia:Array<any>[] = [];
   constructor(public json:JsonService){
     this.json.getJson('https://fortnite-api.com/v2/shop/br/?language=pt-BR').subscribe((res : any)=>{
       console.log(res);
+      this.data = res.data.date;
+      this.skinsDia = res.data.featured.entries[0].items[0].images.smallIcon;
+      console.log(this.skinsDia);
+      
+
+     
     })
 
 
