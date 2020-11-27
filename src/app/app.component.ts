1f16cb2f31f286caaed5987624772248237d9caf
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-11-27 19:49:10 -0300

    getNomes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,31 @@ import { JsonService } from './json.service';
 export class AppComponent {
   title = 'Minha Página';
   data: string = '';
-  skinsDia: Array<any> = [];
+  skinsDestaque: Array<any> = [];
   skins: Array<any> = [];
   skinsFiltradas: any;
+  nomes: any;
+  
+
+
   constructor(public json: JsonService) {
     this.json.getJson('https://fortnite-api.com/v2/shop/br/?language=pt-BR').subscribe((res: any) => {
       console.log(res);
       this.data = res.data.date;
-      this.skinsDia = res.data.featured.entries[0].items[0].images.smallIcon;
+
+      //buscando as imagens das skins
+      this.skinsDestaque = res.data.featured.entries[0].items[0].images.smallIcon;
       this.skins = res.data.featured.entries;
-      this.skins.forEach(novasSkins => {
-        this.skinsFiltradas = novasSkins.items[0].images.smallIcon;
+      this.skins.forEach(destaque => {
+        this.skinsFiltradas = destaque.items[0].images.smallIcon;
         let target = document.getElementById('target');
         let newArrSkin = document.createElement("img");
         newArrSkin.src = this.skinsFiltradas;
         target?.appendChild(newArrSkin);
+//Buscando nomes
+this.nomes = destaque.items[0].name;
+console.log(this.nomes)
+
 
       })
 
