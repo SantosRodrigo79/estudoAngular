6723b6497bab3ce4d245a823e6b5a3354f262e78
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-12-01 16:36:00 -0300

    images

--- src/app/models/loja.model.ts
+++ src/app/models/loja.model.ts
@@ -1,16 +1,15 @@
 export class Loja {
-    data:any;
+    status:any;
+    data: any;
     daily:any;
+    entries:any;
+    category:any;
+    items: any;
+    name:any;
+    images:any;
+    smallIcon:any;
+    description:any;
     featured:any;
-    specialFeatured:any;
-    items:{} = Array();
-    entries:Array<any>= [];
-    id:string = '';
-    date: any;
-    finalPrice:number = 0;
-    regularPrice: number =0;
-    description:string ='';
-    name:string ='';
 
 
 }
