c9691cda1c1b9eae30def54adc55f317f6b5135a
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-12-02 12:16:52 -0300

    correcoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GerarListasComponent } from './listas/gerar-listas/gerar-listas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path :'',
+  component:GerarListasComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
