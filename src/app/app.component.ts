375f1b71c3adb197ccbcf5087c7559461eaf406a
Rodrigo Souza Santos <digo_denovo@hotmail.com>
2020-12-02 17:08:12 -0300

    correcaoComponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { JsonService } from './json.service';
 
 @Component({
   selector: 'app-root',
@@ -7,39 +6,15 @@ import { JsonService } from './json.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Fortnite Loja de Itens';
-  data: string = '';
-  skins: Array<any> = [];
-  skinsFiltradas: any;
+  title = '';
+
   
   
-  constructor(public json: JsonService) {
-    this.json.getJson().subscribe((res: any) => {
-      console.log(res);
-      this.data = res.data.date;
-
-      //buscando as imagens das skins
-     
-      this.skins = res.data.featured.entries;
-      this.skins.forEach(destaque => {
-        this.skinsFiltradas = destaque.items[0].images.smallIcon;
-        let images = document.getElementById('Images');
-        let newArrSkin = document.createElement("img");
-        newArrSkin.src = this.skinsFiltradas;
-        images?.appendChild(newArrSkin);
-//Buscando nomes
-let nomesArr = document.getElementById('nomes');
-let nomesLista = document.createElement('div');
-nomesLista.appendChild(document.createTextNode(destaque.items[0].name));
-nomesArr?.appendChild(nomesLista);
-
-      })
-
-    })
+  constructor() {
+ 
 
 
-  }
-
+   }
 }
 
 
